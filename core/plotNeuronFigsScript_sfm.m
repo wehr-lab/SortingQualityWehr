537a784172d65a8944c778277dbb63ab52972b1f
samfind <70771925+samfind@users.noreply.github.com>
2021-12-14 13:57:36 -0800

    archived original script
    saved the original script for reference and made a new one for me

--- core/plotNeuronFigsScript_sfm.m
+++ core/plotNeuronFigsScript_sfm.m
@@ -1,8 +1,10 @@
-SNR = []; SNR1 = [];
-ksRoot = pwd;
+SNR = []; 
+SNR1 = [];
+ksRoot = pwd;                                                               % Set dir to analyze
 
-loadPars.loadPCs = true;
-sp = loadKSdir(ksRoot, loadPars);
+loadPars.loadPCs = false;
+loadPars.excludeNoise = true;
+sp = loadKSdir(ksRoot, loadPars);                                           % see loadKSdir for varargin
 
 inclCID = sp.cids(sp.cgs == 2);
 st = sp.st;
@@ -12,28 +14,31 @@ pcFeat = sp.pcFeat;
 pcFeatInd = sp.pcFeatInd;
 spikeAmps = sp.tempScalingAmps;
 figDir = fullfile(ksRoot, 'figs'); 
-if ~exist(figDir,'dir'); mkdir(figDir); end
+if ~exist(figDir, 'dir') 
+    mkdir(figDir); 
+end
 
 params.dataType = sp.dtype;
 params.filename = sp.dat_path;
-d = dir(fullfile(ksRoot,params.filename)); 
+d = dir(fullfile(ksRoot, params.filename)); 
 
 
 nSamp = d.bytes/2/sp.n_channels_dat;
 params.dataSize = [sp.n_channels_dat nSamp];
 params.chanMap = readNPY(fullfile(ksRoot, 'channel_map.npy'));
 params.Fs = sp.sample_rate;
-params.xcoords = sp.xcoords; params.ycoords = sp.ycoords;
+params.xcoords = sp.xcoords; 
+params.ycoords = sp.ycoords;
 params.plotDistance = 100;
 params.nWFsToLoad = 1000;
 params.nWFsToPlot = 100;
-params.gain = 0.6/512/500*1e6; % raw file units to uV
+params.gain = 0.6/512/500 * 1e6;                                            % raw file units to uV
 params.nPCsToPlot = 50000;
 % params.highlightRegions = [132 1104];
 
 %% extract median WFs (just once)
 cd(ksRoot)
-inclSP = ismember(clu, sp.cids(sp.cgs==2));
+inclSP = ismember(clu, sp.cids(sp.cgs == 2));
 medWFs = extractMedianWFs(clu(inclSP), st(inclSP), params.Fs, params.filename, ...
     params.dataType, params.dataSize, params.chanMap, params.gain);
 
@@ -56,23 +61,24 @@ sparsePCfeat = sparsePCs(pcFeat, pcFeatInd, sp.spikeTemplates, 2, 10);
 %%
 figDir = fullfile(sprintf('%s',pwd),'\figs');
 
-theseISI = isiV(cgs==2);
-theseCR = cR(cgs==2);
-theseID = uQ(cgs==2);
-maxChans=[];
+theseISI = isiV(cgs == 2);
+theseCR = cR(cgs == 2);
+theseID = uQ(cgs == 2);
+maxChans = [];
 for q = 1:length(inclCID)
     clusterID = inclCID(q);
-    stats.medWF = squeeze(medWFs(inclCID==clusterID,:,:))';
-    stats.isiContamination = theseISI(inclCID==clusterID);
-    stats.isoDistance = theseID(inclCID==clusterID);
-    stats.mahalContamination = theseCR(inclCID==clusterID);
+    stats.medWF = squeeze(medWFs(inclCID == clusterID,:,:))';
+    stats.isiContamination = theseISI(inclCID == clusterID);
+    stats.isoDistance = theseID(inclCID == clusterID);
+    stats.mahalContamination = theseCR(inclCID == clusterID);
     [figHand, maxChan, snr, snr1] = neuronFig(clusterID, st, clu, sparsePCfeat, spikeAmps, stats, params);
     set(figHand, 'Position', [-1890         -59        1810        1031]);
     savefig(figHand, fullfile(figDir, sprintf('/cluster%d', clusterID)))
-    close(figHand); clear figHand
-    maxChans(q)=maxChan(1);
-    SNR(q)=snr;
-    SNR1(q)=snr1;
+    close(figHand); 
+    clear figHand
+    maxChans(q) = maxChan(1);
+    SNR(q) = snr;
+    SNR1(q) = snr1;
 end
 save('maxChans.mat', 'maxChans') %save max spike channels
 save('SNR.mat', 'SNR', 'SNR1') %save signal to noise calculations
@@ -83,7 +89,7 @@ figure; plotDriftmap(spikeTimes, spikeAmps, spikeDepths);
 
 %plot basic frequency characterization
 lfpD = pwd; % LFP file from spikeGLX specifically
-fn1=dir('*.dat');
+fn1 = dir('*.dat');
 lfpFilename = fn1.name;
 
 lfpFs = 30e3;  % neuropixels phase3a
